f658baa082ff62f24d3e85ce8899504d9401287f
Kelvin Luck <git@kelvinluck.com>
2013-04-14 23:36:02 +0100

    Using require.js to load the site javascript - time to get more organised

--- src/js/nif.js
+++ src/js/nif.js
@@ -1,37 +1,43 @@
-$(
+define(
+  [],
   function()
   {
-    $('#map-toggle a').on(
-      'click',
-      function(e)
+    $(
+      function()
       {
-        var map = $('#map'),
-            isOpen = map.is('.is-open');
-        map[isOpen ? 'removeClass' : 'addClass']('is-open');
-        if (!isOpen) {
-          setTimeout(initMap, 1000);
+        $('#map-toggle a').on(
+          'click',
+          function(e)
+          {
+            var map = $('#map'),
+              isOpen = map.is('.is-open');
+            map[isOpen ? 'removeClass' : 'addClass']('is-open');
+            if (!isOpen) {
+              setTimeout(initMap, 1000);
+            }
+            return false;
+          }
+        );
+
+        function initMap()
+        {
+          $('#map>p').remove();
+
+          var map = L.map('map').setView([51.505, -0.09], 13);
+
+          L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          }).addTo(map);
+
+          L.marker([51.5, -0.09]).addTo(map)
+            .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
+            .openPopup();
         }
-        return false;
+
+        if ($('#map.is-open').length) {
+          initMap();
+        }
       }
     );
-
-    function initMap()
-    {
-      $('#map>p').remove();
-
-      var map = L.map('map').setView([51.505, -0.09], 13);
-
-      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
-
-      L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
-        .openPopup();
-    }
-
-    if ($('#map.is-open').length) {
-      initMap();
-    }
   }
 );
