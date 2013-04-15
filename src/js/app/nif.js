166428c9d2e1a17353ffc1b30b9240f8ffd06a78
Kelvin Luck <git@kelvinluck.com>
2013-04-15 23:45:02 +0100

    Cleaner way of setting the map tiles?

--- src/js/app/nif.js
+++ src/js/app/nif.js
@@ -24,25 +24,34 @@ define(
           $('#map>p').remove();
 
           var map = L.map('map').setView([0, 0], 2);
+
           L.tileLayer(
-            'http://tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png',
+            'http://{s}.tiles.mapbox.com/v3/{user}.{map}/{z}/{x}/{y}.png',
             {
-              "attribution": "<a href='http://mapbox.com/about/maps' target='_blank'>Terms & Feedback</a>",
-              "bounds": [-180, - 85, 180, 85],
-              "center": [0, 0, 3],
-              "data": ["http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/markers.geojsonp"],
-              "geocoder": "http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/geocode/{query}.jsonp",
-              "id": "vitch.map-knaif0fn",
-              "maxzoom": 17,
-              "minzoom": 0,
-              "name": "Simple terrain",
-              "private": true,
-              "scheme": "xyz",
-              "tilejson": "2.0.0",
-              "tiles": ["http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png", "http://b.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png", "http://c.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png", "http://d.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png"],
-              "webpage": "http://tiles.mapbox.com/vitch/map/map-knaif0fn"
+              user: 'vitch',
+              map: 'map-knaif0fn'
             }
           ).addTo(map);
+
+//          L.tileLayer(
+//            'http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png',
+//            {
+//              "attribution": "<a href='http://mapbox.com/about/maps' target='_blank'>Terms & Feedback</a>",
+//              "bounds": [-180, - 85, 180, 85],
+//              "center": [0, 0, 3],
+//              "data": ["http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/markers.geojsonp"],
+//              "geocoder": "http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/geocode/{query}.jsonp",
+//              "id": "vitch.map-knaif0fn",
+//              "maxzoom": 17,
+//              "minzoom": 0,
+//              "name": "Simple terrain",
+//              "private": true,
+//              "scheme": "xyz",
+//              "tilejson": "2.0.0",
+//              "tiles": ["http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png", "http://b.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png", "http://c.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png", "http://d.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png"],
+//              "webpage": "http://tiles.mapbox.com/vitch/map/map-knaif0fn"
+//            }
+//          ).addTo(map);
 
 
         }
