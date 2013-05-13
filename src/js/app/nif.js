75f578e9f4a50bc9cedc623d8f0fd2fe78c4f3d2
Kelvin Luck <git@kelvinluck.com>
2013-05-13 17:00:12 +0700

    Using a Backbone view to deal with the logic for the map...

--- src/js/app/nif.js
+++ src/js/app/nif.js
@@ -1,73 +1,7 @@
 define(
-  [],
-  function()
-  {
-    $(
-      function()
-      {
-        $('#map-toggle a').on(
-          'click',
-          function(e)
-          {
-            var map = $('#map'),
-              isOpen = map.is('.is-open');
-            map[isOpen ? 'removeClass' : 'addClass']('is-open');
-            if (!isOpen) {
-              setTimeout(initMap, 1000);
-            }
-            return false;
-          }
-        );
-
-        function initMap()
-        {
-          $('#map>p').remove();
-
-          var map = L.map('map').setView([0, 0], 2);
-
-          L.tileLayer(
-            'http://{s}.tiles.mapbox.com/v3/{user}.{map}/{z}/{x}/{y}.png',
-            {
-              user: 'nakeditchyfeet',
-              map: 'map-9xnn0a7i'
-            }
-          ).addTo(map);
-
-//          L.tileLayer(
-//            'http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png',
-//            {
-//              "attribution": "<a href='http://mapbox.com/about/maps' target='_blank'>Terms & Feedback</a>",
-//              "bounds": [-180, - 85, 180, 85],
-//              "center": [0, 0, 3],
-//              "data": ["http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/markers.geojsonp"],
-//              "geocoder": "http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/geocode/{query}.jsonp",
-//              "id": "vitch.map-knaif0fn",
-//              "maxzoom": 17,
-//              "minzoom": 0,
-//              "name": "Simple terrain",
-//              "private": true,
-//              "scheme": "xyz",
-//              "tilejson": "2.0.0",
-//              "tiles": ["http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png", "http://b.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png", "http://c.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png", "http://d.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png"],
-//              "webpage": "http://tiles.mapbox.com/vitch/map/map-knaif0fn"
-//            }
-//          ).addTo(map);
-
-
-        }
-
-        if ($('#map.is-open').length) {
-          initMap();
-        }
-
-        setTimeout(
-          function()
-          {
-            $('#map').addClass('is-animated');
-          },
-          1000
-        )
-      }
-    );
+  ['views/MapView'],
+  function (MapView) {
+    'use strict';
+    var mapView = new MapView();
   }
 );
---
+++ src/js/app/views/MapView.js
@@ -0,0 +1,77 @@
+define(
+  [
+  ],
+  function () {
+    'use strict';
+
+    return Backbone.View.extend(
+      {
+        el: '#map-panel',
+        events: {
+          'click #map-toggle a': 'onMapToggle'
+        },
+        initialize: function () {
+
+          this.isOpen = this.$el.is('.is-open');
+          if (this.isOpen) {
+            this.initMap();
+          }
+
+          // Wait until the page has rendered before allowing CSS animations on the
+          _.delay(
+            _.bind(function () {
+              this.$el.addClass('is-animated');
+            }, this),
+            1000
+          );
+        },
+        initMap: function () {
+          if (this.isMapInitialised) {
+            return;
+          }
+          this.isMapInitialised = true;
+
+          var mapContainer = this.$('#map').empty()[0],
+              map = L.map(mapContainer).setView([0, 0], 2);
+
+          L.tileLayer(
+            'http://{s}.tiles.mapbox.com/v3/{user}.{map}/{z}/{x}/{y}.png',
+            {
+              user: 'nakeditchyfeet',
+              map: 'map-9xnn0a7i'
+            }
+          ).addTo(map);
+
+//          L.tileLayer(
+//            'http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png',
+//            {
+//              "attribution": "<a href='http://mapbox.com/about/maps' target='_blank'>Terms & Feedback</a>",
+//              "bounds": [-180, - 85, 180, 85],
+//              "center": [0, 0, 3],
+//              "data": ["http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/markers.geojsonp"],
+//              "geocoder": "http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/geocode/{query}.jsonp",
+//              "id": "vitch.map-knaif0fn",
+//              "maxzoom": 17,
+//              "minzoom": 0,
+//              "name": "Simple terrain",
+//              "private": true,
+//              "scheme": "xyz",
+//              "tilejson": "2.0.0",
+//              "tiles": ["http://a.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png", "http://b.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png", "http://c.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png", "http://d.tiles.mapbox.com/v3/vitch.map-knaif0fn/{z}/{x}/{y}.png"],
+//              "webpage": "http://tiles.mapbox.com/vitch/map/map-knaif0fn"
+//            }
+//          ).addTo(map);
+        },
+        onMapToggle: function (e) {
+          this.isOpen = !this.isOpen;
+          this.$el[this.isOpen ? 'addClass' : 'removeClass']('is-open');
+
+          if (this.isOpen) {
+            this.initMap();
+          }
+          return false;
+        }
+      }
+    );
+  }
+);
