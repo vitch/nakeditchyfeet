08549051323dca5ab17afdb0e2070939a310cf07
Kelvin Luck <git@kelvinluck.com>
2013-07-27 19:23:35 +0800

    Added names to the middlewares so that they display nicely in the haggerston report

--- src/middleware/order.js
+++ src/middleware/order.js
@@ -3,7 +3,7 @@ module.exports = function () {
 
   // We need to make sure that the list pages are rendered after the content pages otherwise the intro
   // filter will fail. Hacky workaround?
-  return function (pages, next, options) {
+  return function order(pages, next, options) {
     var sortedPages = pages.sort(function(p1, p2) {
       var o1 = p1.renderOrder || 0;
       var o2 = p2.renderOrder || 0;
--- src/middleware/photosets.js
+++ src/middleware/photosets.js
@@ -5,7 +5,7 @@ var Page = require('grunt-haggerston/tasks/lib/page');
 module.exports = function () {
   'use strict';
 
-  return function (pages, next, options) {
+  return function photosets(pages, next, options) {
     var photosets = grunt.file.readJSON('src/data/photos/photosets.json');
     async.each(photosets, function (photoset, cb) {
       var photosetData = grunt.file.readJSON('src/data/photos/' + photoset.id + '.json');
