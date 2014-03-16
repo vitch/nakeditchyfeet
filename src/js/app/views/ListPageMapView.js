6a17ebfac38224df8b2c38624ae5df32ffe0fcdd
Kelvin Luck <git@kelvinluck.com>
2014-03-15 23:01:54 -1000

    Rendering a map onto the list page for every flight...

---
+++ src/js/app/views/ListPageMapView.js
@@ -0,0 +1,47 @@
+define(
+  [
+  ],
+  function () {
+    'use strict';
+
+    return Backbone.View.extend(
+      {
+        initialize: function (options) {
+            var data = this.$el.data();
+            this.initMap([data.fromLat, data.fromLng], [data.toLat, data.toLng]);
+        },
+        initMap: function (from, to) {
+
+          var mapContainer = this.$el.empty()[0];
+
+          var leafletMap = L.map(mapContainer, {
+            dragging: false,
+            touchZoom: false,
+            scrollWheelZoom: false,
+            doubleClickZoom: false,
+            boxZoom: false,
+            tap: false,
+            trackResize: false,
+            keyboard: false,
+            zoomControl: false,
+            attributionControl: false,
+            layers:[]
+          });
+
+          L.tileLayer(
+            'http://{s}.tiles.mapbox.com/v3/{user}.{map}/{z}/{x}/{y}.png',
+            {
+              user: 'nakeditchyfeet',
+              map: 'map-9xnn0a7i'
+            }
+          ).addTo(leafletMap);
+
+
+          var line = L.polyline([from, to]).addTo(leafletMap);
+          leafletMap.fitBounds(line.getBounds());
+
+        },
+      }
+    );
+  }
+);
