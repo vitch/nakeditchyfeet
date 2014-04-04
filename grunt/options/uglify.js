981f035d5997d37b412e9cbd3f8d0bd2883500b0
Kelvin Luck <git@kelvinluck.com>
2014-04-03 23:48:09 -0500

    Using a minified version of require in production. Should probably make an almond build of site code instead and not include require at all...

--- grunt/options/uglify.js
+++ grunt/options/uglify.js
@@ -1,8 +1,19 @@
 module.exports = function(grunt) {
   return {
     libs: {
+      options: {
+        preserveComments: 'some'
+      },
       files: {
         'out/js/libs.min.js': grunt.jsLibs
+      }
+    },
+    require: {
+      options: {
+        preserveComments: 'some'
+      },
+      files: {
+        'out/js/lib/require.js': 'vendor/requirejs/require.js'
       }
     }
   };
