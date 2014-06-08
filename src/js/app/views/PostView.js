cd9819dd70b7dd2e245a9bf1c76979ccc62feb60
Kelvin Luck <git@kelvinluck.com>
2014-06-07 21:30:58 -0300

    Fixing the map in the PostView

--- src/js/app/views/PostView.js
+++ src/js/app/views/PostView.js
@@ -12,21 +12,20 @@ define(
         initialize: function (options) {
           if (this.$el.length) {
 
-            this.sideMapView = options.sideMapView;
-            var marker = L.marker(
-                      this.$el.data('geo').split(','),
-                      {
-                        title: this.$('>h1').text(),
-                        icon: L.AwesomeMarkers.icon({
-                          icon: 'book', // FIXME - should include the correct icon in the page and grab it from there...
-                          prefix: 'nif-icon',
-                          markerColor: 'darkblue',
-                          className: 'awesome-marker'
-                        })
-                      }
-                    );
-
-            this.sideMapView.setMarker(marker);
+            options.sideMapView.initMap({
+              marker: L.marker(
+                this.$el.data('geo').split(','),
+                {
+                  title: this.$('>h1').text(),
+                  icon: L.AwesomeMarkers.icon({
+                    icon: 'book', // FIXME - should include the correct icon in the page and grab it from there...
+                    prefix: 'nif-icon',
+                    markerColor: 'darkblue',
+                    className: 'awesome-marker'
+                  })
+                }
+              )
+            })
 
           }
         }
