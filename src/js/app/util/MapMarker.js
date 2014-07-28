35c0aec87435d506df93072e32056a03ee9fa04c
Kelvin Luck <git@kelvinluck.com>
2014-07-28 20:02:08 +0100

    Nicer looking way of forcing the tooltips out to a decent width

--- src/js/app/util/MapMarker.js
+++ src/js/app/util/MapMarker.js
@@ -6,7 +6,7 @@ define(
     'use strict';
 
     return MarkerBase.extend({
-      
+
       initialize: function (latlng, options) {
         options.icon = L.AwesomeMarkers.icon({
           icon: options.iconClass,
@@ -14,6 +14,13 @@ define(
           markerColor: 'darkblue',
           className: 'awesome-marker'          
         });
+
+        if (options.title) {
+          // Hacky way to push the tooltips out to a decent width...
+          var t1 = options.title.substr(0, 20);
+          var t2 = options.title.substr(20);
+          options.title = t1.replace(/ /g, '&nbsp;') + t2;
+        }
 
         L.Marker.prototype.initialize.call(this, latlng, options);
       }
