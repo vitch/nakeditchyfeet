0e616f34e8f4a1a9caff5c4a50537a43847bd88d
Kelvin Luck <git@kelvinluck.com>
2014-07-29 16:28:20 +0100

    Embeding the .woff font so we don't need to worry about caching and the fact the file is recreated each time with a different name...

--- grunt/options/webfont.js
+++ grunt/options/webfont.js
@@ -11,6 +11,7 @@ module.exports = {
         classPrefix: 'nif-icon-',
         mixinPrefix: 'nif-icon-'
       },
+      embed: true,
       // fontHeight: 512,
       // descent: 2,
       // ascent: 2,
